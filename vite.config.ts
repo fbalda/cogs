e320a1eb0d281446daafd19e7cca7fc08b161cdd
Felix Balda <private@felixbalda.com>
2024-01-24 12:46:45 -0300

    chore: cd/ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+// /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-});
+export default () => {
+  return defineConfig({
+    plugins: [svelte()],
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    base: process.env.BASE_URL ?? "/",
+  });
+};
